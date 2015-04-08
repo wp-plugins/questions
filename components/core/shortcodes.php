8aff35bbb2ce50ae38d75f6c98fd69e33a8a5f3e
awesome-ug <awesome-ug@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-08 15:22:56 +0000

    Minimum length of 0 is also possible now
    git-svn-id: https://plugins.svn.wordpress.org/questions/tags/1.0.0 beta 4@1130499 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ components/core/shortcodes.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Questions Shortcodes
+ *
+ * This should be used as parent class for Question-Answers.
+ *
+ * @author awesome.ug, Author <support@awesome.ug>
+ * @package Questions/Core
+ * @version 1.0.0
+ * @since 1.0.0
+ * @license GPL 2
+
+  Copyright 2015 awesome.ug (support@awesome.ug)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ */
+
+if ( !defined( 'ABSPATH' ) ) exit;
+
+class QuestionsShortCodes{
+	var $tables;
+	var $components = array();
+	var $question_types = array();
+	
+	public function __construct(){
+		add_shortcode( 'questions', array( $this, 'questions' ) );
+	}
+	
+	public function questions( $atts ){
+		global $Questions_ProcessResponse;
+		extract( shortcode_atts( array(
+				'id' => '',
+				'title' => __( 'Survey', 'questions-locale' )
+			),
+			$atts ) );
+		
+		if( '' === $id ):
+			_e( 'Please enter an id in the survey shortcode!', 'questions-locale' );
+			return;
+		endif;
+		
+		echo $Questions_ProcessResponse->get_survey( $id );
+	}
+}
+$QuestionsShortCodes = new QuestionsShortCodes();
