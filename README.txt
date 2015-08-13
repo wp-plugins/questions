6006387ba1f9d9e35720d28f7d8dd0b6b73b8d32
awesome-ug <awesome-ug@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 16:52:04 +0000

    Switched to beta 19
    git-svn-id: https://plugins.svn.wordpress.org/questions/tags/1.0.0 beta 19@1220104 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,176 @@
+=== Questions ===
+Contributors: mahype, awesome-ug
+Donate link: http://awesome.ug/donate
+Tags: survey, surveys, polls, poll, create poll, custom poll, online poll, custom survey, online survey, votes, voting, wp polls, wp survey, yop poll, online survey, online poll, survey form, data collection, questions
+Requires at least: 4.1.1
+Tested up to: 4.3.0
+Stable tag: 1.0.0
+
+Drag & drop your survey in the WordPress way!
+
+== Description ==
+
+>**Drag & drop your survey in the WordPress way!**
+>
+>It never felt better to create your survey like with this plugin. Do your survey and own the survey data. This is not software as a service, Questions is a totally independent and free plugin.
+
+**Elements to drop**
+
+* **Text** - Simple text input.
+* **Textarea** - Multiple line text input.
+* **One Choice** - Radiobutton one choice input.
+* **Multiple Choice** - Select one ore more values with this input.
+* **Dropdown** - Select your answer from a dropdown input.
+
+**Features**
+
+* **Drag&Drop** - Drag&drop questions to your survey, also sort answers by drag&drop.
+* **Timerange** - Give surveys a start and an end date.
+* **Bar Chart Results** - Showing results after participating or with shortcodes.
+* **Validation** - Validate the entered data in question settings.
+* **Participiants** - Add registered users to survey or let everybody participate.
+* **Remember Mail** - Send a remembering Email to participiants.
+* **Lightweight CSS** - Easy to overwrite CSS in frontend.
+* **CVS Export** - Get your results as CVS file.
+
+**Languages**
+
+* English
+* German
+* Dutch (Thanks to [@remcow])
+* Italian (Thanks to Giovanni Simiani)
+* Persian (Thanks to Hos3in)
+* Russian (Thanks to [@kosm])
+* Swedish (Thanks to [@elle_])
+
+>Feel free to add your own language! We will add it to the code if you send us language files in your language.
+
+**Bug Reporting**
+
+Please report issues at Github:
+
+* https://github.com/awsmug/Questions/issues
+
+This is an Awesome Plugin.
+
+twitter: http://twitter.com/awsmug - GitHub: https://github.com/awsmug
+
+== Installation ==
+
+1. Upload `questions` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. In the left menu appears the `Surveys` button
+
+== Screenshots ==
+
+1. **Drag&Drop your questions and move them up and down**
+2. **Also Drag&Drop answers**
+3. **Published survey**
+4. **Results of a survey**
+5. **Inviting WordPress users or let open who can participate**
+6. **Text element setting options**
+7. **Setup templates for inviting and remembering mails**
+
+== Changelog ==
+
+= 1.0.0 beta 19 =
+* Added russian language files (Thanks to [@kosm])
+* Added italian language files (Thanks to Giovanni Simiani)
+* No export if no results
+* Refactored code
+* Also dublicating terms for form
+
+= 1.0.0 beta 18 =
+* Added timerange
+* Added usernames to export
+* Reworked admin component
+* Fixed settings page
+* Fixed UTF8 bug on exporting surveys
+* Removed save function from bulk editing
+* Fixed bug on participating with selected members
+
+= 1.0.0 beta 17 =
+* Bettered up Chart view (Switched back to Dimple)
+* Added result charts to admin
+* Ordering charts like ordered in admin
+* Added body class on Questions pages
+* Added WP Nonce check to forms
+
+= 1.0.0 beta 16 =
+* Changed URL to Chartjs because name of file was not uppercase.
+
+= 1.0.0 beta 15 =
+* Only showing steps if there is more then one step
+* Results can be deleted
+* Stripping slashes on exporting CSV
+* Showing error messages if there is no answer given
+* Switched to Charts.js
+* Refactored file structure
+
+= 1.0.0 beta 14 =
+* Added missing columns on export if an answer was not given
+* Removed WP Editor from Settings API and descriptions because of massive problems with WP Editor and jQuery droppables.
+
+= 1.0.0 beta 13 =
+* Making Questions Multisite-Ready
+* Flushing rewrite rules correct
+* Added WP Editor to settings API
+* Changed description editor to WP Editor
+* Moved description text under question, before fields
+* Fixed bug on double description output
+* Bettered up Plugin CSS
+* Enhanced code structure
+
+= 1.0.0 beta 12 =
+* Fixed exporting bug on exporting multiple choice fields results
+* Enhanced code structure
+
+= 1.0.0 beta 11 =
+* Added message on reaching PHP max_num_fields
+* Enhanced code structure
+
+= 1.0.0 beta 10 =
+* Enhanced code structure
+* Added Dutch language files (Thanks to [@remcow])
+* Also showing questions if question text not have been filled in
+* Reworked Drag&Drop area to be sortable from the first drop
+* Preparing data after submitting
+
+= 1.0.0 beta 9 =
+* Fixed restrictions bug
+
+= 1.0.0 beta 8 =
+* Added pariticipiants option "No restrictions"
+* Added filters for default on $participiants_restrictions
+* Added filters for texts
+* Added swedish language files (Thanks to [@elle_])
+* Added persian language files (Thanks to Hos3in)
+* Code enhancements (Thanks to [@bueltge])
+
+= 1.0.0 beta 7 =
+* Fixed problems with errors on further steps
+* Made (int) for steps
+* Returning shortcode content and not echo it
+* Changed collation of tables to utf8_general_ci
+* Changed to more secure getting path function
+* Fixed text mistake on separator
+
+= 1.0.0 beta 6 =
+* Fixed result charts bug
+* Fixed some translation fails
+
+= 1.0.0 beta 5 =
+* Added Questions Shortcode for embedding surveys
+* Fixed some translation fails
+
+= 1.0.0 beta 4 =
+* Min length of 0 is possible too now
+
+= 1.0.0 beta 3 =
+* Added bar charts and shortcodes for showing results
+
+= 1.0.0 beta 2 =
+* Fixed bug after sending form as participiant
+
+= 1.0.0 beta =
+* Fist official 1.0.0 beta
---
+++ functions.php
@@ -0,0 +1,303 @@
+<?php
+/**
+ * Helper functions for plugin
+ * @author awesome.ug, Author <support@awesome.ug>
+ * @package Questions/Core
+ * @version 1.0.0
+ * @since 1.0.0
+ * @license GPL 2
+
+Copyright 2015 awesome.ug (support@awesome.ug)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/*
+* Getting Plugin Template
+* @since 1.0.0
+*/
+if ( defined( 'QUESTIONS_FOLDER' ) ):
+	function qu_locate_template( $template_names, $load = FALSE, $require_once = TRUE ) {
+
+		$located = locate_template( $template_names, $load, $require_once );
+
+		if ( '' == $located ):
+			foreach ( ( array ) $template_names as $template_name ):
+				if ( ! $template_name ) {
+					continue;
+				}
+				if ( file_exists( QUESTIONS_FOLDER . '/templates/' . $template_name ) ):
+					$located = QUESTIONS_FOLDER . '/templates/' . $template_name;
+					break;
+				endif;
+			endforeach;
+		endif;
+
+		if ( $load && '' != $located ) {
+			load_template( $located, $require_once );
+		}
+
+		return $located;
+	}
+endif;
+
+/**
+ * Getting standard mailtext strings
+ * @param string $mailtext_title Type of mailtext which have to be chosen
+ * @return string $mailtext Mailtext as String
+ */
+function qu_get_mail_template_text( $mailtext_title ) {
+
+	$text = '';
+	switch ( $mailtext_title ) {
+		case 'thankyou_participating':
+			$text = stripslashes( get_option( 'questions_thankyou_participating_text_template' ) );
+			if ( empty( $text ) ):
+				$text = esc_attr__(
+					'Dear %username%,
+
+thank you for participating on the survey "%survey_title%". Your survey data was saved successfully.
+
+Best regards,
+
+%site_name%', 'questions-locale'
+				);
+			endif;
+
+			break;
+
+		case 'invitation':
+			$text = stripslashes( get_option( 'questions_invitation_text_template' ) );
+			if ( empty( $text ) ):
+				$text = esc_attr__(
+					'Dear %username%,
+
+you have been invited to participate to the survey "%survey_title%". Participate here:
+
+%survey_url%
+
+Best regards,
+
+%site_name%', 'questions-locale'
+				);
+			endif;
+
+			break;
+
+		case 'reinvitation':
+			$text = stripslashes( get_option( 'questions_reinvitation_text_template' ) );
+			if ( empty( $text ) ):
+				$text = esc_attr__(
+					'Dear %username%,
+
+the survey "%survey_title%" is not finished yet. Please fill out and finish the survey. Participate here:
+
+%survey_url%
+
+Best regards,
+
+%site_name%', 'questions-locale'
+				);
+			endif;
+
+			break;
+
+	}
+
+	return $text;
+}
+
+/**
+ * Getting standard mailsubject strings
+ * @param string $mailtext_title Type of mail subject which have to be chosen
+ * @return string $mailtext Mail subject as String
+ */
+function qu_get_mail_template_subject( $mailsubject_title ) {
+
+	$text = '';
+	switch ( $mailsubject_title ) {
+		case 'thankyou_participating':
+			$text = stripslashes( get_option( 'questions_thankyou_participating_subject_template' ) );
+			if ( empty( $text ) ):
+				$text = esc_attr__( 'Thank you for participating!', 'questions-locale' );
+			endif;
+
+			break;
+
+		case 'invitation':
+			$text = stripslashes( get_option( 'questions_invitation_subject_template' ) );
+			if ( empty( $text ) ):
+				$text = esc_attr__( 'You are invited to answer a survey', 'questions-locale' );
+			endif;
+
+			break;
+
+		case 'reinvitation':
+			$text = stripslashes( get_option( 'questions_reinvitation_subject_template' ) );
+			if ( empty( $text ) ):
+				$text = esc_attr__( 'Don´t forget to answer the Survey', 'questions-locale' );
+			endif;
+
+			break;
+
+	}
+
+	return $text;
+}
+
+/**
+ * Get mail sender settings
+ * @param $option string Mail sender option to get
+ * @return mixed $setting Setting
+ */
+function qu_get_mail_settings( $option ) {
+
+	$setting = '';
+	switch ( $option ) {
+		case 'from_name':
+			$setting = stripslashes( get_option( 'questions_mail_from_name' ) );
+
+			if ( empty( $setting ) ):
+				$setting = get_option( 'blogname' );
+			endif;
+
+			break;
+
+		case 'from_email':
+			$setting = stripslashes( get_option( 'questions_mail_from_email' ) );
+
+			if ( empty( $setting ) ):
+				$setting = get_option( 'admin_email' );
+			endif;
+
+			break;
+	}
+
+	return $setting;
+}
+
+/**
+ * Shortener to get email return name from options
+ * @return string $from_name "From" name
+ */
+function qu_change_email_return_name() {
+
+	return qu_get_mail_settings( 'from_name' );
+}
+
+/**
+ * Shortener to get email return address from options
+ * @return string $from_email "From" email address
+ */
+function qu_change_email_return_address() {
+
+	return qu_get_mail_settings( 'from_email' );
+}
+
+/**
+ * Own Email function
+ * @param string $to_email Mail address for sending to
+ * @param string $subject Subject of mail
+ * @param string $content
+ * @return bool
+ */
+function qu_mail( $to_email, $subject, $content ) {
+
+	add_filter( 'wp_mail_from_name', 'qu_change_email_return_name' );
+	add_filter( 'wp_mail_from', 'qu_change_email_return_address' );
+
+	$result = wp_mail( $to_email, $subject, $content );
+
+	// Logging
+	$content = str_replace( chr( 13 ), '', strip_tags( $content ) );
+	qu_create_log_entry( array( $to_email, $subject, $content ) );
+
+	remove_filter( 'wp_mail_from_name', 'qu_change_email_return_name' );
+	remove_filter( 'wp_mail_from', 'qu_change_email_return_address' );
+
+	return $result;
+}
+
+/**
+ * Base logging function
+ * @param array $values The values which have to be saved
+ */
+function qu_create_log_entry( $values ) {
+
+	if ( ! is_array( $values ) ) {
+		return;
+	}
+
+	$line = date( 'Y-m-d;H:i:s;' );
+
+	foreach ( $values AS $value ):
+		$line .= $value . ';';
+	endforeach;
+
+	$line = str_replace( array( "\r\n", "\n\r", "\n", "\r" ), ' ', $line );
+
+	$line .= chr( 13 );
+
+	$logdir = WP_CONTENT_DIR . '/logs/';
+
+	if ( ! file_exists( $logdir ) ) {
+		mkdir( $logdir );
+	}
+
+	$logfile = $logdir . 'questions.log';
+
+	$file = fopen( $logfile, 'a' );
+	fwrite( $file, $line );
+	fclose( $file );
+}
+
+/**
+ * Preparing input data
+ * @param string $data
+ * @return string $data 
+ */
+function qu_prepare_post_data( $data ){
+	// Do not preparing objects or arrays	
+	if( is_object( $data ) || is_array( $data ) )
+		return $data;
+	
+	$data = trim( $data );
+	$data = stripslashes( $data );
+	return $data;
+}
+
+/**
+ * Creates a random id
+ * @return string $id ID string
+ */
+function qu_id(){
+    $id = md5( rand() );
+    return $id;
+}
+
+/**
+ * Debugging helper function
+ */
+if( !function_exists( 'p' ) ){
+	function p( $var ){
+		echo '<pre>';
+		print_r( $var );
+		echo '</pre>';
+	}
+}
